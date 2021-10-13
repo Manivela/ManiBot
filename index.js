477662ee7513a6521a478eaf6730a2cd5f1ace19
Yunus Gülcü <yunus.gulcu35@gmail.com>
2021-10-13 21:43:53 +0300

    add ingame roles

--- index.js
+++ index.js
@@ -18,47 +18,73 @@ client.on('ready', async () => {
 //         );
 //     }
 // });
-client.on('presenceUpdate', (_oldPresence, newPresence) => {
-    if (!newPresence) {
-        return;
-    }
-
-    const member = newPresence.member;
+client.on('presenceUpdate', (oldPresence, newPresence) => {
+    const member = oldPresence ? oldPresence.member : newPresence.member;
     if (!member.presence.activities || member.presence.activities.length == 0 || !member.presence.activities.some(a => !!a.applicationId)) {
         // not playing a game
         return;
     }
     newPresence.activities.filter(a => !!a.applicationId).forEach(async activity => {
+        const roleName = activity.name;
         // only add if the user doesn't already have the role
-        if (member.roles.cache.some(role => role.name === activity.name)) {
+        if (hasRole(member, roleName)) {
             if (process.env.NODE_ENV !== 'production') {
-                console.log(`"${member.displayName}" already has role "${activity.name}"`);
+                console.log(`"${member.displayName}" already has role "${roleName}"`);
             }
             return;
         }
-        // search for role in cache
-        let role = newPresence.guild.roles.cache.find(r => r.name === activity.name);
-        if (!role) {
-            // fetch new roles from the server
-            try {
-                role = await newPresence.guild.roles.fetch(roles => {
-                    return roles.find(r => r.name === activity.name);
-                });
-            }
-            catch (error) {
-                console.error('error while fetching roles:', error);
-                return;
-            }
-        }
+        const role = await findRole(newPresence.guild.roles, roleName);
 
         if (!role) {
             if (process.env.NODE_ENV !== 'production') {
-                console.log(`role "${activity.name}" not found on this server.`);
+                console.log(`role "${roleName}" not found on this server.`);
             }
             return;
         }
         member.roles.add(role).then(() => `role "${role.name}" added to "${member.displayName}"`).catch(reason => {
             console.log(`failed to add role "${role.name}" to "${member.name}"! reason: ${reason}`);
+        });
+    });
+    newPresence.activities.filter(a => !!a.applicationId).forEach(async activity => {
+        const inGameRoleName = `${activity.name}-ingame`;
+        // only add if the user doesn't already have the role
+        if (hasRole(member, inGameRoleName)) {
+            if (process.env.NODE_ENV !== 'production') {
+                console.log(`"${member.displayName}" already has role "${inGameRoleName}"`);
+            }
+            return;
+        }
+        const inGameRole = await findRole(newPresence.guild.roles, inGameRoleName);
+
+        if (!inGameRole) {
+            if (process.env.NODE_ENV !== 'production') {
+                console.log(`role "${inGameRoleName}" not found on this server.`);
+            }
+            return;
+        }
+        member.roles.add(inGameRole).then(() => `role "${inGameRole.name}" added to "${member.displayName}"`).catch(reason => {
+            console.log(`failed to add role "${inGameRole.name}" to "${member.name}"! reason: ${reason}`);
+        });
+    });
+    oldPresence.activities.filter(a => !!a.applicationId).forEach(async activity => {
+        const inGameRoleName = `${activity.name}-ingame`;
+        // only remove if the user already has the role
+        if (!hasRole(member, inGameRoleName)) {
+            if (process.env.NODE_ENV !== 'production') {
+                console.log(`"${member.displayName}" doesn't have role "${inGameRoleName}"`);
+            }
+            return;
+        }
+        const inGameRole = await findRole(newPresence.guild.roles, inGameRoleName);
+
+        if (!inGameRole) {
+            if (process.env.NODE_ENV !== 'production') {
+                console.log(`role "${inGameRoleName}" not found on this server.`);
+            }
+            return;
+        }
+        member.roles.remove(inGameRole).then(() => `role "${inGameRole.name}" added to "${member.displayName}"`).catch(reason => {
+            console.log(`failed to remove role "${inGameRole.name}" from "${member.name}"! reason: ${reason}`);
         });
     });
 });
@@ -70,7 +96,7 @@ client.on('voiceStateUpdate', (oldState, newState) => {
     }
     if (newUserChannel !== null) {
         const channelName = `${slugify(newUserChannel.name, { lower: true })}-bot`;
-        const foundChannel = newState.guild.channels.cache.find(channel => channel.type === 'GUILD_TEXT' && channel.name === channelName);
+        const foundChannel = findChannel(newState.guild.channels, channelName);
         if (foundChannel && newUserChannel.parentId === foundChannel.parentId) {
             foundChannel.permissionOverwrites.create(newState.member, { 'VIEW_CHANNEL': true, 'SEND_MESSAGES': true, 'READ_MESSAGE_HISTORY': true })
                 .then(channel => console.log(`added "${newState.member.displayName}" to channel "${channel.name}"`))
@@ -81,15 +107,17 @@ client.on('voiceStateUpdate', (oldState, newState) => {
                 type: 'GUILD_TEXT',
                 permissionOverwrites: [
                     {
+                        // Bot
                         id: client.user.id,
                         allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'],
                     },
                     {
+                        // User that joined the vc
                         id: newState.member.id,
                         allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'],
                     },
                     {
-                        // @everyone role
+                        // @everyone
                         id: newState.guild.roles.everyone.id,
                         deny: ['VIEW_CHANNEL'],
                     },
@@ -101,7 +129,7 @@ client.on('voiceStateUpdate', (oldState, newState) => {
     }
     if (oldUserChannel !== null) {
         const channelName = `${slugify(oldUserChannel.name, { lower: true })}-bot`;
-        const foundChannel = newState.guild.channels.cache.find(channel => channel.type === 'GUILD_TEXT' && channel.name === channelName);
+        const foundChannel = findChannel(newState.guild.channels, channelName);
         if (foundChannel && oldUserChannel.parentId === foundChannel.parentId) {
             if (oldUserChannel.members.size === 0) {
                 foundChannel.delete()
@@ -117,3 +145,29 @@ client.on('voiceStateUpdate', (oldState, newState) => {
         }
     }
 });
+
+function findChannel(serverChannels, channelName) {
+    return serverChannels.cache.find(channel => channel.type === 'GUILD_TEXT' && channel.name === channelName);
+}
+
+async function findRole(serverRoles, roleName) {
+    // search for role in cache
+    let role = serverRoles.cache.find(r => r.name === roleName);
+    if (!role) {
+        // fetch new roles from the server
+        try {
+            role = await serverRoles.fetch(roles => {
+                console.log(`${roleName} not found in cache fetching it from server`);
+                return roles.find(r => r.name === roleName);
+            });
+        }
+        catch (error) {
+            console.error('error while fetching roles:', error);
+            return;
+        }
+    }
+    return role;
+}
+function hasRole(member, roleName) {
+    return member.roles.cache.some(role => role.name === roleName);
+}
